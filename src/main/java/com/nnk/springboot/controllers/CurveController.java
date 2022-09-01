84137af4f9259a463c8a1a0b2e6d0e8f73548dc7
ricar <ricaragas@hotmail.com>
2022-09-01 14:34:57 +0200

    Story 2 (CurvePoint) - completed TODO tasks for POJO, controller and views

--- src/main/java/com/nnk/springboot/controllers/CurveController.java
+++ src/main/java/com/nnk/springboot/controllers/CurveController.java
@@ -1,6 +1,9 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.CurvePoint;
+import com.nnk.springboot.services.CurvePointService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,45 +13,78 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class CurveController {
-    // TODO: Inject Curve Point service
+
+    private static final Logger log = LogManager.getLogger("CurveController");
+
+    // Inject Curve Point service
+    private final CurvePointService service;
+    public CurveController(CurvePointService service) {
+        this.service = service;
+    }
 
     @RequestMapping("/curvePoint/list")
     public String home(Model model)
     {
-        // TODO: find all Curve Point, add to model
+        // find all Curve Point, add to model
+        List<CurvePoint> list = service.readAll();
+        model.addAttribute("list", list);
+        log.info("GET /curvePoint/list - Showing a list of {} curve points", list.size());
         return "curvePoint/list";
     }
 
     @GetMapping("/curvePoint/add")
     public String addBidForm(CurvePoint bid) {
+        log.info("GET /curvePoint/add - Showing form");
         return "curvePoint/add";
     }
 
     @PostMapping("/curvePoint/validate")
     public String validate(@Valid CurvePoint curvePoint, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return Curve list
+        // check data valid and save to db, after saving return Curve list
+        if (result.hasErrors()) {
+            log.info("POST /curvePoint/validate - HAS ERRORS, showing form");
+            return "bidList/add";
+        }
+        service.create(curvePoint);
+        log.info("POST /curvePoint/validate - ADDED 1 new entry, returning to list");
         return "curvePoint/add";
     }
 
     @GetMapping("/curvePoint/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get CurvePoint by Id and to model then show to the form
+        // get CurvePoint by Id and to model then show to the form
+        Optional<CurvePoint> existingObject = service.read(id);
+        if (existingObject.isPresent()) {
+            model.addAttribute("curvePoint", existingObject.get());
+            log.info("GET /curvePoint/update({}) - EXISTS - Showing form", id);
+        }
+        log.info("GET /curvePoint/update({}] - DOES NOT EXIST - returning to list", id);
         return "curvePoint/update";
     }
 
     @PostMapping("/curvePoint/update/{id}")
     public String updateBid(@PathVariable("id") Integer id, @Valid CurvePoint curvePoint,
                              BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update Curve and return Curve list
+        // check required fields, if valid call service to update Curve and return Curve list
+        if (result.hasErrors()) {
+            log.info("POST /curvePoint/UPDATE({}) - HAS ERRORS, showing form", id);
+            return "curvePoint/update/" + id;
+        }
+        service.update(curvePoint);
+        log.info("POST /curvePoint/UPDATE({}) - updated 1 ENTRY, RETURNING TO LIST", id);
         return "redirect:/curvePoint/list";
     }
 
     @GetMapping("/curvePoint/delete/{id}")
     public String deleteBid(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Curve by Id and delete the Curve, return to Curve list
+        // Find Curve by Id and delete the Curve, return to Curve list
+        service.delete(id);
+        log.info("GET /curvePoint/delete({}) - DELETED 1 entry, returning to list", id);
         return "redirect:/curvePoint/list";
     }
 }
--- src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -5,11 +5,91 @@ import org.hibernate.validator.constraints.Length;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.sql.Timestamp;
 
 
 @Entity
 @Table(name = "curvepoint")
 public class CurvePoint {
-    // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "Id")
+    private Integer id;
+
+    @Column(name = "CurveId")
+    @NotNull(message = "{not.null}")
+    @Positive(message = "{positive}")
+    private Integer curveId;
+
+    private Timestamp asOfDate;
+
+    private Double term;
+
+    private Double value;
+
+    private Timestamp creationDate = new Timestamp(System.currentTimeMillis());
+
+    // CONSTRUCTORS
+
+    public CurvePoint() {}
+
+    public CurvePoint(int curveId, double term, double value) {
+        this.curveId = curveId;
+        this.term = term;
+        this.value = value;
+    }
+
+    // GETTERS
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getCurveId() {
+        return curveId;
+    }
+
+    public Timestamp getAsOfDate() {
+        return asOfDate;
+    }
+
+    public Double getTerm() {
+        return term;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public Timestamp getCreationDate() {
+        return creationDate;
+    }
+
+    // SETTERS
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setCurveId(Integer curveId) {
+        this.curveId = curveId;
+    }
+
+    public void setAsOfDate(Timestamp asOfDate) {
+        this.asOfDate = asOfDate;
+    }
+
+    public void setTerm(Double term) {
+        this.term = term;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public void setCreationDate(Timestamp creationDate) {
+        this.creationDate = creationDate;
+    }
 }
