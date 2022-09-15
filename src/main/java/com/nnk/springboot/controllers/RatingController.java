9dfd9c7d5bdb7fcc7a753b0c5321aa92a5117fe2
ricar <ricaragas@hotmail.com>
2022-09-15 18:33:18 +0200

    Story 3 (Rating) - completed TODO tasks for controller and views

--- src/main/java/com/nnk/springboot/controllers/RatingController.java
+++ src/main/java/com/nnk/springboot/controllers/RatingController.java
@@ -1,6 +1,9 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.services.RatingService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,45 +13,78 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class RatingController {
-    // TODO: Inject Rating service
+    private static final Logger log = LogManager.getLogger("RatingController");
+
+    // Inject Rating service
+    private final RatingService service;
+    public RatingController(RatingService service) {
+        this.service = service;
+    }
 
     @RequestMapping("/rating/list")
     public String home(Model model)
     {
-        // TODO: find all Rating, add to model
+        // find all Rating, add to model
+        List<Rating> list = service.readAll();
+        model.addAttribute("list", list);
+        log.info("GET /rating/list - Showing a list of {} ratings", list.size());
         return "rating/list";
     }
 
     @GetMapping("/rating/add")
     public String addRatingForm(Rating rating) {
+        log.info("GET /rating/add - Showing form");
         return "rating/add";
     }
 
     @PostMapping("/rating/validate")
     public String validate(@Valid Rating rating, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return Rating list
-        return "rating/add";
+        // check data valid and save to db, after saving return Rating list
+        if (result.hasErrors()) {
+            log.info("POST /rating/validate - HAS ERRORS, showing form");
+            return "rating/add";
+        }
+        service.create(rating);
+        log.info("POST /rating/validate - ADDED 1 new entry, returning to list");
+        return "redirect:/rating/list";
     }
 
     @GetMapping("/rating/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get Rating by Id and to model then show to the form
-        return "rating/update";
+        // get Rating by Id and to model then show to the form
+        Optional<Rating> existingObject = service.read(id);
+        if (existingObject.isPresent()) {
+            model.addAttribute("rating", existingObject.get());
+            log.info("GET /rating/update({}) - EXISTS - Showing form", id);
+            return "rating/update";
+        }
+        log.info("GET /rating/update({}) - DOES NOT EXIST - returning to list", id);
+        return "redirect:/rating/list";
     }
 
     @PostMapping("/rating/update/{id}")
     public String updateRating(@PathVariable("id") Integer id, @Valid Rating rating,
                              BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update Rating and return Rating list
+        // check required fields, if valid call service to update Rating and return Rating list
+        if (result.hasErrors()) {
+            log.info("POST /rating/update({}) - HAS ERRORS, showing form", id);
+            return "rating/update/" + id;
+        }
+        service.update(rating);
+        log.info("POST /rating/update({}) - UPDATED 1 entry, returning to list", id);
         return "redirect:/rating/list";
     }
 
     @GetMapping("/rating/delete/{id}")
     public String deleteRating(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Rating by Id and delete the Rating, return to Rating list
+        // Find Rating by Id and delete the Rating, return to Rating list
+        service.delete(id);
+        log.info("GET /rating/delete({}) - DELETED 1 entry, returning to list", id);
         return "redirect:/rating/list";
     }
 }
