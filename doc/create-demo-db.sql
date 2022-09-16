ff03d2cfa99eb32e971af662e91ae900585aacbd
ricar <ricaragas@hotmail.com>
2022-09-16 09:39:22 +0200

    now the demo user can delete rows

--- doc/create-demo-db.sql
+++ doc/create-demo-db.sql
@@ -6,4 +6,4 @@ USE demo;
 
 DROP USER IF EXISTS 'poseidon_demo_user';
 CREATE USER 'poseidon_demo_user' IDENTIFIED BY 'demopassword';
-GRANT SELECT, INSERT, UPDATE ON demo.* TO 'poseidon_demo_user';
+GRANT SELECT, INSERT, UPDATE, DELETE ON demo.* TO 'poseidon_demo_user';
