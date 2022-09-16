cc5669fbfc7bd3860aa9d2077f3d45b03f698c6e
ricar <ricaragas@hotmail.com>
2022-09-16 10:15:42 +0200

    added logger to UserController

--- src/main/java/com/nnk/springboot/controllers/UserController.java
+++ src/main/java/com/nnk/springboot/controllers/UserController.java
@@ -2,7 +2,8 @@ package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.User;
 import com.nnk.springboot.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,21 +14,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class UserController {
-    @Autowired
-    private UserService userService;
+
+    private static final Logger log = LogManager.getLogger("UserController");
+
+    // Inject User service
+    private final UserService service;
+    public UserController(UserService service) {
+        this.service = service;
+    }
 
     @RequestMapping("/user/list")
     public String home(Model model)
     {
-        model.addAttribute("users", userService.readAll());
+        List<User> list = service.readAll();
+        model.addAttribute("users", list);
+        log.info("GET /user/list - Showing a list of {} users", list.size());
         return "user/list";
     }
 
     @GetMapping("/user/add")
     public String addUser() {
+        log.info("GET /user/add - Showing form");
         return "user/add";
     }
 
@@ -36,16 +47,19 @@ public class UserController {
         if (!result.hasErrors()) {
             BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
             user.setPassword(encoder.encode(user.getPassword()));
-            userService.create(user);
-            model.addAttribute("users", userService.readAll());
+            service.create(user);
+            log.info("POST /user/validate - ADDED 1 new entry, returning to list");
+            model.addAttribute("users", service.readAll());
             return "redirect:/user/list";
         }
+        log.info("POST /user/validate - HAS ERRORS, showing form");
         return "user/add";
     }
 
     @GetMapping("/user/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        User user = userService.read(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        User user = service.read(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        log.info("GET /user/update({}) - EXISTS - Showing form with empty password", id);
         user.setPassword("");
         model.addAttribute("user", user);
         return "user/update";
@@ -55,22 +69,25 @@ public class UserController {
     public String updateUser(@PathVariable("id") Integer id, @Valid User user,
                              BindingResult result, Model model) {
         if (result.hasErrors()) {
+            log.info("POST /user/update({}) - HAS ERRORS, showing form", id);
             return "user/update";
         }
 
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         user.setPassword(encoder.encode(user.getPassword()));
         user.setId(id);
-        userService.update(user);
-        model.addAttribute("users", userService.readAll());
+        service.update(user);
+        log.info("POST /user/update({}) - UPDATED 1 entry, returning to list", id);
+        model.addAttribute("users", service.readAll());
         return "redirect:/user/list";
     }
 
     @GetMapping("/user/delete/{id}")
     public String deleteUser(@PathVariable("id") Integer id, Model model) {
-        User user = userService.read(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-        userService.delete(user.getId());
-        model.addAttribute("users", userService.readAll());
+        User user = service.read(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        service.delete(user.getId());
+        log.info("GET /user/delete({}) - DELETED 1 entry, returning to list", id);
+        model.addAttribute("users", service.readAll());
         return "redirect:/user/list";
     }
 }
