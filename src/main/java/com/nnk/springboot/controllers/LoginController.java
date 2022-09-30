52c49dc20d6a1bcccb1427eeea9106a98cd9ef65
ricar <ricaragas@hotmail.com>
2022-09-30 21:22:43 +0200

    bug fix - now correctly redirects on successful login and 403 errors

--- src/main/java/com/nnk/springboot/controllers/LoginController.java
+++ src/main/java/com/nnk/springboot/controllers/LoginController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.security.Principal;
+
 @Controller
 @RequestMapping("app")
 public class LoginController {
@@ -30,10 +32,11 @@ public class LoginController {
     }
 
     @GetMapping("error")
-    public ModelAndView error() {
+    public ModelAndView error(Principal principal) {
         ModelAndView mav = new ModelAndView();
         String errorMessage= "You are not authorized for the requested data.";
         mav.addObject("errorMsg", errorMessage);
+        mav.addObject("username", principal.getName());
         mav.setViewName("403");
         return mav;
     }
